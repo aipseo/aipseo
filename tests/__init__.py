b20b295657806269a427c8bbc8c7683c1c773fa8
Mark Counterman <ilovepoker@gmail.com>
2025-05-14 17:13:29 -0500

    Clean up: Remove all development and test code for production release

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# spdx-license-identifier: apache-2.0
-# copyright 2024 mark counterman
-
-"""Test package for aipseo."""
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def test_manifest(temp_path):
-    os.environ["aipseo_manifest"] = temp_path
-
-    # Cleanup
-    os.environ.pop("aipseo_manifest", None) 
